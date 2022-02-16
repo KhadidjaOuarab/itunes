8e16763e6406d60fbbc11ea65338570ffb1db0a9
ouarb khadidja <ouarab.khadidja@gmail.com>
2022-02-16 10:56:10 +0100

    update navbar design

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react'
 import '../App.css'
 import imag from '../Assets/panier.png'
-import {Link } from "react-router-dom";
+import SearchImage from '../Assets/search.png'
+import { Link } from "react-router-dom";
 
-function Navbar({Logo, searchFunction, inputVal, getValue, deleteFunction, position, panierParam }) {
+function Navbar({ Logo, searchFunction, inputVal, getValue, deleteFunction, position, panierParam }) {
 
 
     return (
@@ -14,9 +15,13 @@ function Navbar({Logo, searchFunction, inputVal, getValue, deleteFunction, posit
                 </Link>
                 <div className='navbarInput'>
                     <div className={position}>
-                        <input type='text' className='inputSearch' onChange={getValue} value={inputVal} />
-                        <input type='submit' value='Effacer' onClick={deleteFunction} className='effacer' />
-                        <input type='submit' value='Rechercher' className='effacer' onClick={searchFunction} />
+                        <input type='text' placeholder="Search songs.." className='inputSearch' onChange={getValue} value={inputVal} />
+                        <button type='submit' className='delete' onClick={deleteFunction}  >
+                          X
+                        </button>
+                        <button type='submit' className='search'  onClick={searchFunction} >
+                            <img src={SearchImage} />
+                        </button>
                     </div>
 
                     <button onClick={panierParam} >
